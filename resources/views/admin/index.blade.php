4bed0af21fe2e2afc75b5050a2f135d1546b9bfe
Thanh Ha <ha.phan@asiantech.vn>
2018-05-15 10:13:52 +0700

    xoa the du thua

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -1,6 +1,6 @@
 @extends('admin.layouts.admin')
 
-@section('title', 'Admin Borrow Book')
+@section('title', trans('label.title.index'))
 
 @section('content')
 
@@ -234,126 +234,6 @@
               </div>
             </div>
             <!-- ./col -->
-        </div>
-        <!-- /.row -->
-
-      <!-- =========================================================== -->
-
-        <h2 class="page-header">Social Widgets</h2>
-
-        <div class="row">
-            <div class="col-md-4">
-                <!-- Widget: user widget style 1 -->
-                <div class="box box-widget widget-user-2">
-                    <!-- Add the bg color to the header using any of the bg-* classes -->
-                    <div class="widget-user-header bg-yellow">
-                        <div class="widget-user-image">
-                            <img class="img-circle" src="bower_components/admin-lte/dist/img/user7-128x128.jpg" alt="User Avatar">
-                        </div>
-                        <!-- /.widget-user-image -->
-                        <h3 class="widget-user-username">Nadia Carmichael</h3>
-                        <h5 class="widget-user-desc">Lead Developer</h5>
-                    </div>
-                    <div class="box-footer no-padding">
-                        <ul class="nav nav-stacked">
-                            <li><a href="#">Projects <span class="pull-right badge bg-blue">31</span></a></li>
-                            <li><a href="#">Tasks <span class="pull-right badge bg-aqua">5</span></a></li>
-                            <li><a href="#">Completed Projects <span class="pull-right badge bg-green">12</span></a></li>
-                            <li><a href="#">Followers <span class="pull-right badge bg-red">842</span></a></li>
-                        </ul>
-                    </div>
-                </div>
-                <!-- /.widget-user -->
-            </div>
-            <!-- /.col -->
-            <div class="col-md-4">
-                <!-- Widget: user widget style 1 -->
-                <div class="box box-widget widget-user">
-                    <!-- Add the bg color to the header using any of the bg-* classes -->
-                    <div class="widget-user-header bg-aqua-active">
-                        <h3 class="widget-user-username">Alexander Pierce</h3>
-                        <h5 class="widget-user-desc">Founder &amp; CEO</h5>
-                    </div>
-                    <div class="widget-user-image">
-                        <img class="img-circle" src="bower_components/admin-lte/dist/img/user1-128x128.jpg" alt="User Avatar">
-                    </div>
-                    <div class="box-footer">
-                        <div class="row">
-                            <div class="col-sm-4 border-right">
-                                <div class="description-block">
-                                    <h5 class="description-header">3,200</h5>
-                                    <span class="description-text">SALES</span>
-                                </div>
-                                <!-- /.description-block -->
-                            </div>
-                            <!-- /.col -->
-                            <div class="col-sm-4 border-right">
-                                <div class="description-block">
-                                    <h5 class="description-header">13,000</h5>
-                                    <span class="description-text">FOLLOWERS</span>
-                                </div>
-                                <!-- /.description-block -->
-                            </div>
-                            <!-- /.col -->
-                            <div class="col-sm-4">
-                                <div class="description-block">
-                                    <h5 class="description-header">35</h5>
-                                    <span class="description-text">PRODUCTS</span>
-                                </div>
-                                <!-- /.description-block -->
-                            </div>
-                            <!-- /.col -->
-                        </div>
-                        <!-- /.row -->
-                    </div>
-                </div>
-                <!-- /.widget-user -->
-            </div>
-            <!-- /.col -->
-            <div class="col-md-4">
-                <!-- Widget: user widget style 1 -->
-                <div class="box box-widget widget-user">
-                    <!-- Add the bg color to the header using any of the bg-* classes -->
-                    <div class="widget-user-header bg-black" style="background: url('bower_components/admin-lte/dist/img/photo1.png') center center;">
-                      <h3 class="widget-user-username">Elizabeth Pierce</h3>
-                      <h5 class="widget-user-desc">Web Designer</h5>
-                    </div>
-                    <div class="widget-user-image">
-                        <img class="img-circle" src="bower_components/admin-lte/dist/img/user3-128x128.jpg" alt="User Avatar">
-                    </div>
-                    <div class="box-footer">
-                        <div class="row">
-                            <div class="col-sm-4 border-right">
-                                <div class="description-block">
-                                    <h5 class="description-header">3,200</h5>
-                                    <span class="description-text">SALES</span>
-                                </div>
-                                <!-- /.description-block -->
-                            </div>
-                            <!-- /.col -->
-                            <div class="col-sm-4 border-right">
-                                <div class="description-block">
-                                    <h5 class="description-header">13,000</h5>
-                                    <span class="description-text">FOLLOWERS</span>
-                                </div>
-                                <!-- /.description-block -->
-                            </div>
-                            <!-- /.col -->
-                            <div class="col-sm-4">
-                                <div class="description-block">
-                                    <h5 class="description-header">35</h5>
-                                    <span class="description-text">PRODUCTS</span>
-                                </div>
-                                <!-- /.description-block -->
-                            </div>
-                            <!-- /.col -->
-                        </div>
-                        <!-- /.row -->
-                    </div>
-                </div>
-                <!-- /.widget-user -->
-            </div>
-            <!-- /.col -->
         </div>
         <!-- /.row -->
 
