54be54cb28d73096e4ef5ec08d4f50f84f8626c9
Hay Tran <haytranngoc@gmail.com>
2018-05-17 13:19:51 +0700

    unit test show list books

--- tests/Browser/Pages/Backend/ShowListBookTest.php
+++ tests/Browser/Pages/Backend/ShowListBookTest.php
@@ -6,6 +6,7 @@ use Tests\DuskTestCase;
 use Laravel\Dusk\Browser;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 use App\Models\Book;
+use Faker\Factory as Faker;
 
 class ShowListBookTest extends DuskTestCase
 {
@@ -13,6 +14,16 @@ class ShowListBookTest extends DuskTestCase
 
     const NUMBER_RECORD_CREATE = 20;
     const RECORD_LIMIT = 15;
+
+    /**
+    * Override function setUp() for make user login
+    *
+    * @return void
+    */
+    public function setUp()
+    {
+        factory(Book::class, self::NUMBER_RECORD_CREATE)->create();
+    }
 
     /**
      * A Dusk test show list book.
