9566bcf7f50ae75626d7cff72e5e0d7c69e03a88
at-nale <na.le@asiantech.vn>
2018-05-17 09:40:03 +0700

    fix view update.blade.php

---
+++ app/Http/Requests/UpdateUserRequest.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\Request;
+
+class UpdateUserRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'name'            => 'required|string|max:255',
+            'identity_number' => 'required|integer|unique:users,identity_number,',
+            'avatar'          => 'image|mimes:png,jpg,jpeg',
+            'dob'             => 'date_format:"Y-m-d"',
+            'address'         => 'string|max:255',
+        ];
+    }
+}
