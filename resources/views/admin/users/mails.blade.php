db037713d3358598b3fb1300b04f0d75f11c5989
Thanh Ha <ha.phan@asiantech.vn>
2018-05-17 10:48:25 +0700

    fix template mails

--- resources/views/admin/users/mails.blade.php
+++ resources/views/admin/users/mails.blade.php
@@ -1,13 +1,4 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<title>Borrow Book</title>
-	<meta charset="utf-8">
-</head>
-<body>
-	<h1>Mail from Borrow Book</h1>
-	<h3>Create user success</h3>
-	<p>Email: {{$email}}</p>
-	<p>Password: {{$password}}</p>
-</body>
-</html>
+<h1>Mail from Borrow Book</h1>
+<h3>Create user success</h3>
+<p>Email: {{$email}}</p>
+<p>Password: {{$password}}</p>
