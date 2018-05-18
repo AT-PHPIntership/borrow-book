e5f50d010892674331496cea451dd2e4247a3e7e
Hay Tran <haytranngoc@gmail.com>
2018-05-18 11:15:41 +0700

    layout update book

--- app/Http/Controllers/Admin/BookController.php
+++ app/Http/Controllers/Admin/BookController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Admin;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Models\Book;
+use App\Models\Category;
 
 class BookController extends Controller
 {
@@ -17,5 +18,20 @@ class BookController extends Controller
     {
         $books = Book::with('imageBooks')->paginate();
         return view('admin.books.index', compact('books', $books));
+    }
+
+    /**
+     * Show layout of book.
+     *
+     * @param int $id id of book
+     *
+     * @return view
+     */
+    public function edit($id)
+    {
+        $book = Book::with(['category', 'imageBooks'])->findOrFail($id);
+        $categories = Category::all();
+        $data = compact('book', 'categories');
+        return view('admin.books.edit', $data);
     }
 }
---
+++ resources/views/admin/books/edit.blade.php
@@ -0,0 +1,78 @@
+@extends('admin.layouts.admin')
+
+@section('title', trans('book.list_book'))
+
+@section('content')
+
+<div class="content-wrapper">
+    <!-- Content Header (Page header) -->
+    
+    <section class="content-header">
+        <div class="box box-primary">
+            <div class="box-header with-border">
+                <h3 class="box-title">{{trans('book.create')}}</h3>
+            </div>
+            @include('admin.layouts.partials.errors')
+            <!-- /.box-header -->
+            <!-- form start -->
+            <form role="form" action="{{ route('admin.books.store') }}" method="POST" enctype="multipart/form-data">
+                <input type="hidden" name="_token" value="{{csrf_token()}}">
+                <div class="box-body">
+                    <img src="" alt="">
+                    <div class="form-group">
+                        <label>{{trans('book.form.title_inputs.image')}}</label>
+                        <input type="file" name="photos[]" multiple>
+                    </div>
+                    <div class="form-group">
+                        <label>{{trans('book.form.title_inputs.category')}}</label>
+                        <select class="form-control" id="category" name="category_id">
+                            @foreach ($categories as $category)
+                            <option value="{{ $category->id }}" {{ $book->category->id == $category->id ? 'selected="selected"' : '' }}>{{ $category->name }}</option>
+                            @endforeach
+                        </select>
+                    </div>
+                    <div class="form-group">
+                        <label>{{trans('book.form.title_inputs.title')}}</label>
+                        <input type="text" value="{{ $book->title }}" class="form-control" id="title" name="title" placeholder="{{trans('book.form.placeholders.title')}}">
+                    </div>
+                    <div class="form-group">
+                        <label>{{trans('book.form.title_inputs.description')}}</label>
+                        <div class="form-group">
+                            <textarea name="description" id="description" cols="140" rows="10" placeholder="{{trans('book.form.placeholders.description')}}">{{ $book->description }}</textarea>
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <label>{{trans('book.form.title_inputs.number_of_page')}}</label>
+                        <input type="number" value="{{ $book->number_of_page }}" class="form-control" id="number_of_page" name="number_of_page" placeholder="{{trans('book.form.placeholders.number_of_page')}}">
+                    </div>
+                    <div class="form-group">
+                        <label>{{trans('book.form.title_inputs.author')}}</label>
+                        <input type="text" value="{{ $book->author }}" class="form-control" id="author" name="author" placeholder="{{trans('book.form.placeholders.author')}}">
+                    </div>
+                    <div class="form-group">
+                        <label>{{trans('book.form.title_inputs.publishing_year')}}</label>
+                        <input type="date" value="{{ $book->publishing_year }}" class="form-control" id="publishing_year" name="publishing_year">
+                    </div>
+                    <div class="form-group">
+                        <label>{{trans('book.form.title_inputs.language')}}</label>
+                        <input type="text" value="{{ $book->language }}" class="form-control" id="language" name="language" placeholder="{{trans('book.form.placeholders.language')}}">
+                    </div>
+                    <div class="form-group">
+                        <label>{{trans('book.form.title_inputs.quantity')}}</label>
+                        <input type="number" value="{{ $book->quantity }}" class="form-control" id="quantity" name="quantity" placeholder="{{trans('book.form.placeholders.quantity')}}">
+                    </div>
+                </div>
+                <!-- /.box-body -->
+                <div class="box-footer">
+                    <button type="submit" class="btn btn-primary" id="submit" name="submit">{{trans('book.form.buttons.submit')}}</button>
+                    <button type="reset" class="btn" id="reset" name="reset">{{trans('book.form.buttons.reset')}}</button>
+                </div>
+            </form>
+          </div>
+    </section>
+
+    <!-- Main content -->
+    
+    <!-- /.content -->
+</div>
+@endsection
