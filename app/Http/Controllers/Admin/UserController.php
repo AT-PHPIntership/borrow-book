1507bd21d68d193c8a9fbbe3664c7eb2579d673a
Thanh Ha <ha.phan@asiantech.vn>
2018-05-18 09:49:30 +0700

    fix convention

--- app/Http/Controllers/Admin/UserController.php
+++ app/Http/Controllers/Admin/UserController.php
@@ -86,7 +86,8 @@ class UserController extends Controller
     /**
     * Remove the specified resource from storage.
     *
-    * @param  int  $id
+    * @param int $id id
+    *
     * @return Response
     */
     public function destroy($id)
