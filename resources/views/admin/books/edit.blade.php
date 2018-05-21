b8d9c1001d80a8a549216ca9a78bf986e9eeeb51
Hay Tran <haytranngoc@gmail.com>
2018-05-21 09:31:42 +0700

    create layout update book

--- resources/views/admin/books/edit.blade.php
+++ resources/views/admin/books/edit.blade.php
@@ -17,7 +17,6 @@
             <!-- form start -->
             <form role="form" action="" method="POST" enctype="multipart/form-data">
                 <input type="hidden" name="_token" value="{{csrf_token()}}">
-                
                 {{method_field('PATCH')}}
                 <div class="box-body">
                     <img src="" alt="">
