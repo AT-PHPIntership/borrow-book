191aaf41180e93ba1e0711b3d44ba2abeb342b87
Thanh Ha <ha.phan@asiantech.vn>
2018-05-23 17:01:48 +0700

    search user

--- resources/views/admin/users/index.blade.php
+++ resources/views/admin/users/index.blade.php
@@ -17,8 +17,8 @@
         <div class="row">
             <div class="col-md-10">
                 <div class="search-container">
-                    <form action="/action_page.php">
-                        <input type="text" placeholder="Search" name="search">
+                    <form action="{{route('admin.users.index')}}" method="GET">
+                        <input type="text" placeholder="Search" name="search" required>
                         <button type="submit"><i class="fa fa-search"></i></button>
                     </form>
                 </div>
