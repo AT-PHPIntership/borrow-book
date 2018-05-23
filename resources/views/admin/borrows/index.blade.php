cfd6faddd972bea632981f63612c7a8e1c8589d7
Hay Tran <haytranngoc@gmail.com>
2018-05-23 11:00:58 +0700

    get and show list borrow book

--- resources/views/admin/borrows/index.blade.php
+++ resources/views/admin/borrows/index.blade.php
@@ -17,14 +17,24 @@
                 <table class="table box">
                     <thead>
                         <tr>
+                            <th>{{trans('borrow.table_head.avatar')}}</th>
                             <th>{{trans('borrow.table_head.borrower')}}</th>
                             <th>{{trans('borrow.table_head.number_book')}}</th>
-                            <th>{{trans('borrow.table_head.form_date')}}</th>
+                            <th>{{trans('borrow.table_head.from_date')}}</th>
                             <th>{{trans('borrow.table_head.to_date')}}</th>
-                            <th>{{trans('borrow.table_head.options')}}</th>
+                            <th>{{trans('borrow.table_head.status')}}</th>
                         </tr>
                     </thead>
                     <tbody>
+                        @foreach ($borrows as $borrow)
+                            <tr>
+                                <td><img class="text-center a" src="{{ $borrow->user->avatar_url}}" alt=""></td>
+                                <td>{{ $borrow->user->name }}</td>
+                                <td>{{ $borrow->number_book }}</td>
+                                <td>{{ date('d-m-Y', strtotime($borrow->form_date)) }}</td>
+                                <td>{{ date('d-m-Y', strtotime($borrow->to_date)) }}</td>
+                            </tr>
+                        @endforeach
                     </tbody>
                 </table>
                 <div class="text-center">
