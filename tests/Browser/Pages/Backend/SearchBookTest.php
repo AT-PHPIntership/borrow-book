47971f59b9c02fbe806d051f62f2a70c6aa720de
Thanh Ha <ha.phan@asiantech.vn>
2018-05-25 11:18:52 +0700

    fix comment

--- tests/Browser/Pages/Backend/SearchBookTest.php
+++ tests/Browser/Pages/Backend/SearchBookTest.php
@@ -12,6 +12,9 @@ class SearchBookTest extends DuskTestCase
 {
     use DatabaseMigrations;
 
+    const NUMBER_RECORD_CREATE = 20;
+    const RECORD_LIMIT = 15;
+
     /**
     * Override function setUp() for make book login
     *
@@ -21,21 +24,10 @@ class SearchBookTest extends DuskTestCase
     {
         parent::setUp();
         
-        factory(Category::class, 2)->create();
+        factory(Category::class, 5)->create();
+        factory(Book::class, self::NUMBER_RECORD_CREATE)->create();
         factory(Book::class)->create([
-        	'id' => 1,
-            'category_id' => 2,
-            'title' => 'Title for book',
-            'description' => 'Lorem ABC',
-            'number_of_page' => 1000,
-            'author' => 'ABC',
-            "publishing_year" => '2012-11-22',
-            "language" => 'English',
-            'quantity' => 2,
-            'count_rate' => 0
-        ]);
-        factory(Book::class)->create([
-        	'id' => 2,
+            'id' => 21,
             'category_id' => 1,
             'title' => 'Flower',
             'description' => 'Lorem ABC',
@@ -61,7 +53,7 @@ class SearchBookTest extends DuskTestCase
                 ->click('.button-search-book')
                 ->assertSee('List Book');
             $elements = $browser->elements('.table tbody tr');
-            $this->assertCount(2, $elements);
+            $this->assertCount(self::RECORD_LIMIT, $elements);
         });
     }
 
@@ -78,7 +70,8 @@ class SearchBookTest extends DuskTestCase
                 ->assertSee('List Book')
                 ->type('search', 'Flower')
                 ->click('.button-search-book')
-                ->assertSee('List Book');
+                ->assertSee('List Book')
+                ->assertSee('Found 1 results');
             $elements = $browser->elements('.table tbody tr');
             $this->assertCount(1, $elements);
         });
@@ -100,6 +93,26 @@ class SearchBookTest extends DuskTestCase
                 ->assertSee('List Book');
             $elements = $browser->elements('.table tbody tr');
             $this->assertCount(0, $elements);
+        });
+    }
+
+    /**
+     * Test view Admin List Borrow with pagination
+     *
+     * @return void
+     */
+    public function tesSearchPagination()
+    {
+        $this->browse(function (Browser $browser) {
+            $browser->loginAs($this->user)
+                ->visit('/admin/books')
+                ->assertSee('List Book')
+                ->type('search', 'a')
+                ->click('.button-search-book')
+                ->assertSee('List Book');
+            $paginate_element = $browser->elements('.pagination li');
+            $number_page = count($paginate_element) - 2;
+            $this->assertTrue($number_page == ceil((self::NUMBER_RECORD_CREATE) / (self::RECORD_LIMIT)));
         });
     }
 }
