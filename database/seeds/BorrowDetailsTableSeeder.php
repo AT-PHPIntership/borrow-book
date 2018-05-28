fa5c4efb313da2d7984797d982fc50491784dc51
Thanh Ha <ha.phan@asiantech.vn>
2018-05-28 16:11:02 +0700

    fix spaces

--- database/seeds/BorrowDetailsTableSeeder.php
+++ database/seeds/BorrowDetailsTableSeeder.php
@@ -12,12 +12,12 @@ class BorrowDetailsTableSeeder extends Seeder
     public function run()
     {
         DB::statement('SET FOREIGN_KEY_CHECKS = 0');
-    	for ($i = 1; $i <= 20; $i++) {
+        for ($i = 1; $i <= 20; $i++) {
 
-	        factory(App\Models\BorrowDetail::class)->create([
-	        	'borrow_id' => $i
-	        ]);
-    	}
+            factory(App\Models\BorrowDetail::class)->create([
+                'borrow_id' => $i
+            ]);
+        }
         DB::statement('SET FOREIGN_KEY_CHECKS = 1');
     }
 }
