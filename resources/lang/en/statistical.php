b5fa75abc0921808050c6bbd327dd83698711828
Thanh Ha <ha.phan@asiantech.vn>
2018-05-30 08:16:04 +0700

    layout

---
+++ resources/lang/en/statistical.php
@@ -0,0 +1,19 @@
+<?php
+    return [
+        'time' => [
+            'total' => 'Total',
+            'weekly' => 'Weekly',
+            'monthly' => 'Monthly',
+            'annually' => 'Annually',
+        ],
+        'title' =>[
+            'users' => 'Users',
+            'posts' => 'Posts',
+            'books' => 'Books',
+            'borrowes' => 'Borrowes',
+            'new_users' => 'New Users',
+            'new_posts' => 'New Posts',
+            'new_books' => 'New Books',
+            'new_borrowes' => 'New Borrows',
+        ],
+    ];
--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -7,6 +7,125 @@
 <div class="content-wrapper">
     <!-- Content Header (Page header) -->
     <section class="content-header">
+        <h3>{{ tran}}</h3>
+        <div class="row">
+            <div class="col-md-3 col-sm-6 col-xs-12">
+                <div class="info-box">
+                    <span class="info-box-icon bg-aqua"><i class="fa fa-user"></i></span>
+                    <div class="info-box-content">
+                        <span class="info-box-text">Users</span>
+                        <span class="info-box-number">1,410</span>
+                    </div>
+                    <!-- /.info-box-content -->
+                </div>
+                <!-- /.info-box -->
+            </div>
+            <!-- /.col -->
+            <div class="col-md-3 col-sm-6 col-xs-12">
+                <div class="info-box">
+                    <span class="info-box-icon bg-green"><i class="ion-ios-book-outline"></i></span>
+                    <div class="info-box-content">
+                        <span class="info-box-text">Books</span>
+                        <span class="info-box-number">410</span>
+                    </div>
+                    <!-- /.info-box-content -->
+                </div>
+              <!-- /.info-box -->
+            </div>
+            <!-- /.col -->
+            <div class="col-md-3 col-sm-6 col-xs-12">
+                <div class="info-box">
+                    <span class="info-box-icon bg-yellow"><i class="ion-document-text"></i></span>
+                    <div class="info-box-content">
+                        <span class="info-box-text">Posts</span>
+                        <span class="info-box-number">13,648</span>
+                    </div>
+                    <!-- /.info-box-content -->
+                </div>
+                <!-- /.info-box -->
+            </div>
+            <!-- /.col -->
+            <div class="col-md-3 col-sm-6 col-xs-12">
+                <div class="info-box">
+                    <span class="info-box-icon bg-red"><i class="ion ion-clipboard"></i></span>
+                    <div class="info-box-content">
+                        <span class="info-box-text">Borrowes</span>
+                        <span class="info-box-number">93,139</span>
+                    </div>
+                    <!-- /.info-box-content -->
+                </div>
+                <!-- /.info-box -->
+            </div>
+            <!-- /.col -->
+        </div>
+
+        <h3>WEEKLY</h3>
+        <div class="row">
+            <div class="col-lg-3 col-xs-6">
+                <!-- small box -->
+                <div class="small-box bg-aqua">
+                    <div class="inner">
+                        <h3>150</h3>
+                        <p>New Users</p>
+                    </div>
+                    <div class="icon">
+                        <i class="ion ion-person-add"></i>
+                    </div>
+                    <a href="#" class="small-box-footer">
+                        More info <i class="fa fa-arrow-circle-right"></i>
+                    </a>
+                </div>
+            </div>
+            <!-- ./col -->
+            <div class="col-lg-3 col-xs-6">
+                <!-- small box -->
+                <div class="small-box bg-green">
+                    <div class="inner">
+                        <h3>53<sup style="font-size: 20px">%</sup></h3>
+                        <p>New Borrowes</p>
+                    </div>
+                    <div class="icon">
+                        <i class="ion ion-clipboard"></i>
+                    </div>
+                    <a href="#" class="small-box-footer">
+                        More info <i class="fa fa-arrow-circle-right"></i>
+                    </a>
+                </div>
+            </div>
+            <!-- ./col -->
+            <div class="col-lg-3 col-xs-6">
+                <!-- small box -->
+                <div class="small-box bg-yellow">
+                    <div class="inner">
+                        <h3>44</h3>
+                        <p>News Posts</p>
+                    </div>
+                    <div class="icon">
+                        <i class="ion-document-text"></i>
+                    </div>
+                    <a href="#" class="small-box-footer">
+                        More info <i class="fa fa-arrow-circle-right"></i>
+                    </a>
+                </div>
+            </div>
+            <!-- ./col -->
+            <div class="col-lg-3 col-xs-6">
+                <!-- small box -->
+                <div class="small-box bg-red">
+                    <div class="inner">
+                        <h3>65</h3>
+                        <p>New Books</p>
+                    </div>
+                    <div class="icon">
+                        <i class="ion-ios-book-outline"></i>
+                    </div>
+                    <a href="#" class="small-box-footer">
+                        More info <i class="fa fa-arrow-circle-right"></i>
+                    </a>
+                </div>
+            </div>
+            <!-- ./col -->
+        </div>
     </section>
 
     <!-- Main content -->
