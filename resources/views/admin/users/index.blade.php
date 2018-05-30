a3596311d2773b292e8b3b22c11c2e5d20dfc467
Hay Tran <haytranngoc@gmail.com>
2018-05-30 13:05:56 +0700

    write unit test show detail and history borrow book

--- resources/views/admin/users/index.blade.php
+++ resources/views/admin/users/index.blade.php
@@ -40,13 +40,14 @@
                             <td>{{ $user->email }}</td>
                             <td>
                                 @if($user->role == 0)
-                                <a href="{{ route('admin.users.edit', $user->id) }}" class="btn btn-primary btn-flat fa fa-pencil button-edit"></a>&nbsp;&nbsp;
+                                <a href="{{ route('admin.users.edit', $user->id) }}" class="btn btn-primary btn-flat fa fa-pencil button-edit"></a>
                                 <form method="POST" action="{{ route('admin.users.destroy', $user->id) }}" class="inline form-delete">
                                     {{ csrf_field() }}
                                     {{ method_field('DELETE') }}
                                     <button class="btn btn-danger btn-flat fa fa-trash-o btn-delete-item button-delete" onclick="return confirm('{{trans('user.messages.confirm.delete')}}')"></button>
                                 </form>
                                 @endif
+                                <a href="{{ route('admin.users.show', $user->id) }}" class="btn btn-primary btn-flat fa fa-info button-info"></a>
                             </td>
                         </tr>
                         @endforeach
---
+++ tests/Browser/Pages/Backend/ShowDetailUserTest.php
@@ -0,0 +1,91 @@
+<?php
+
+namespace Tests\Browser\Pages\Backend;
+
+use Tests\DuskTestCase;
+use Laravel\Dusk\Browser;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use DB;
+use App\Models\User;
+use App\Models\Book;
+use App\Models\Borrow;
+use App\Models\Category;
+use App\Models\BorrowDetail;
+use Faker\Factory as Faker;
+
+class ShowDetailUserTest extends DuskTestCase
+{
+    use DatabaseMigrations;
+    
+    /**
+    * Override function set up database
+    *
+    * @return void
+    */
+    public function setUp()
+    {
+        $faker = Faker::create();
+        parent::setUp();
+        factory(User::class)->create([
+            'id' => 2,
+            'email' => 'haytranngoc@gmail.com',
+            'address' => 'ABCXYZ A'
+        ]);
+        factory(Category::class, 2)->create();
+        factory(Book::class, 2)->create();
+        factory(Borrow::class)->create();
+        $borrowId = DB::table('borrowes')->pluck('id')->all();
+        $bookId = DB::table('books')->pluck('id')->all();
+        factory(BorrowDetail::class)->create([
+            'borrow_id' => $faker->randomElement($borrowIds),
+            'book_id' => $faker->randomElement($bookIds)
+        ]);
+
+    }
+
+    /**
+     * A Dusk test click button detail user
+     *
+     * @return void
+     */
+    public function testClickButtonDetailUser()
+    {
+        $user = User::findOrFail(2);
+        $this->browse(function (Browser $browser) use ($user){
+            $browser->loginAs($this->user)
+                ->visit('/admin/users')
+                ->click('.table tbody tr:nth-child(2) .button-info')
+                ->assertPathIs('/admin/users/'.$user->id)
+                ->assertSee('User Detail');
+        });
+    }
+
+    /**
+     * A Dusk test Show Detail User
+     *
+     * @return void
+     */
+    public function testShowDetailUser()
+    {
+        $user = User::findOrFail(2);
+        $borrowes = Borrow::with('borrowDetails.book')
+                        ->where('user_id', $user->id)
+                        ->orderBy('status', 'asc')->get();
+        $this->browse(function (Browser $browser) use ($user, $borrowes) {
+            $browser->loginAs($this->user)
+                ->visit('/admin/users/' . $user->id)
+                ->pause(1000);
+            $this->assertTrue($browser->text('.name') === $user->name);
+            $this->assertTrue($browser->text('.email') === $user->email);
+            $this->assertTrue($browser->text('.identity-number') === $user->identity_number);
+            $this->assertTrue($browser->text('.dob') === $user->dob);
+            $this->assertTrue($browser->text('.address') === $user->address);
+            $this->assertTrue($browser->text('.address') === $user->address);
+            foreach ($borrowes as $borrow) {
+                $this->assertTrue($browser->text('.from-date') === $borrow->form_date);
+                $this->assertTrue($browser->text('.to-date') === $borrow->to_date);
+                $this->assertTrue($browser->value('.status') === $borrow->status);
+            }
+        });
+    }
+}
