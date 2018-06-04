dc0f5858e45c2c811851c8f392065e7c7c36be27
at-nale <na.le@asiantech.vn>
2018-06-04 13:41:11 +0700

    unitest sort book

--- tests/Browser/Pages/Backend/SortBookTest.php
+++ tests/Browser/Pages/Backend/SortBookTest.php
@@ -7,13 +7,12 @@ use Laravel\Dusk\Browser;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 use App\Models\Book;
 use App\Models\Category;
-use Illuminate\Support\Facades\DB;
 use Faker\Factory as Faker;
 
 class SortBookTest extends DuskTestCase
 {
     use DatabaseMigrations;
-
+    
     /**
     * Override function set up database
     *
@@ -22,12 +21,9 @@ class SortBookTest extends DuskTestCase
     public function setUp()
     {
         parent::setUp();
+        
         factory(Category::class,2)->create();
-        $categoryIds = DB::table('categories')->pluck('id')->all();
-        $faker = Faker::create();
-        factory(Book::class,16)->create([
-            'category_id' => $faker->randomElement($categoryIds),
-        ]);
+        factory(Book::class,17)->create();
     }
 
     /*
@@ -62,38 +58,73 @@ class SortBookTest extends DuskTestCase
         return [
             ['title', 'books.title', 2],
             ['author', 'books.author', 3],
-            ['quantity', 'books.quantity', 5]
+            ['quantity','books.quantity', 5],        
         ];
     }
 
     /**
-     * A Dusk test data sort list user by name
-     * 
+     * A Dusk test Sort List Books.
+     *
      * @dataProvider dataForTest
      *
      * @return void
      */
-    public function testSortListBook($name, $order, $columnIndex)
+    public function testSortListBooks($name,$order, $columIndex)
     {
-        $this->browse(function (Browser $browser) use ($order, $name, $columnIndex) {
+        $this->browse(function (Browser $browser) use ($name,$order, $columIndex) {
             $browser->loginAs($this->user)
                 ->visit('admin/books')
-                ->resize(1200,1600)
-                ->click("#book-sort-$name a");
-            //Test list user Desc
-            $arrayAsc =Book::orderBy($order,'asc')->pluck($name)->toArray();
-            for ($i = 1; $i <= 15; $i) {
-                $selector = "#table-index tbody tr:nth-child($i) td:nth-child($columnIndex)";
-                $this->assertEquals($browser->text($selector), $arrayAsc[$i - 1]);
+                ->resize(1200, 1600)
+                ->press("#book-sort-$name a");
+            // Test list Asc
+            $arrAsc =Book::with('category')->orderBy($order,'asc')->pluck($name)->toArray();
+            for ($i = 1; $i <= 15; $i++) {
+                $selector = "#table-index tbody tr:nth-child($i) td:nth-child($columIndex)";
+                $this->assertEquals($browser->text($selector), $arrAsc[$i - 1]);
             }
-            //Test list user Desc
-           /* $browser->click("#book-sort-$name a");
-            $arrayDesc = DB::table('books')->orderBy($order,'desc')->pluck($name)->toArray();
-            for ($i = 1; $i <= 15; $i) {
-                $selector = "#table-index tbody tr:nth-child($i) td:nth-child($columnIndex)";
-                $this->assertEquals($browser->text($selector), $arrayDesc[$i - 1]);
-            }*/
+            // Test list Desc
+            $browser->press("#book-sort-$name a");
+            $arrDesc = Book::with('category')->orderBy($order,'desc')->pluck($name)->toArray();
+            for ($i = 1; $i <= 15; $i++) {
+                $selector = "#table-index tbody tr:nth-child($i) td:nth-child($columIndex)";
+                $this->assertEquals($browser->text($selector), $arrDesc[$i - 1]);
+            }
         });
     }
-    
+
+    /**
+     * A Dusk test sort list books when panigate.
+     *
+     * @dataProvider dataForTest
+     *
+     * @return void
+     */
+    public function testSortListBooksWhenPanigate($name, $order, $columIndex)
+    {
+        $this->browse(function (Browser $browser) use ($name, $order, $columIndex) {
+            $browser->loginAs($this->user)
+                ->visit('admin/books')
+                ->resize(1200, 1600)
+                ->click("#book-sort-$name a")
+                ->clickLink("2");
+            // Test list Asc
+            $arrAsc = Book::with('category')->orderBy($order,'asc')->pluck($name)->toArray();
+            $arraySortAsc = array_chunk($arrAsc, 15)[1];
+            for ($i = 1; $i <= 2; $i++) {
+                $selector = "#table-index tbody tr:nth-child($i) td:nth-child($columIndex)";
+                $this->assertEquals($browser->text($selector), $arraySortAsc[$i - 1]);
+            }
+            // Test list Desc
+            $browser->press("#book-sort-$name a")
+                    ->clickLink("2");
+            $arrAsc = Book::with('category')->orderBy($order,'desc')->pluck($name)->toArray();
+            $arraySortDesc = array_chunk($arrAsc, 15)[1];
+            for ($i = 1; $i <= 2; $i++) {
+                $selector = "#table-index tbody tr:nth-child($i) td:nth-child($columIndex)";
+                $this->assertEquals($browser->text($selector), $arraySortDesc[$i - 1]);
+            }
+        });
+    }
+
+   
 }
