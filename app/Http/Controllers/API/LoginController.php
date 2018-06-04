92889bc7661bbb1b613f9e4330be5874daf81f45
Hay Tran <haytranngoc@gmail.com>
2018-06-04 14:10:12 +0700

    fix unauthenticated

--- app/Http/Controllers/API/LoginController.php
+++ app/Http/Controllers/API/LoginController.php
@@ -16,7 +16,7 @@ class LoginController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function login() 
+    public function login()
     {
         if (Auth::attempt(['email' => request('email'), 'password' => request('password')])) {
             $user = Auth::user();
@@ -30,6 +30,8 @@ class LoginController extends Controller
     /**
      * Register api
      *
+     * @param \Illuminate\Http\Request $request request
+     *
      * @return \Illuminate\Http\Response
      */
     public function register(Request $request)
@@ -41,11 +43,9 @@ class LoginController extends Controller
             'identity_number' => 'required|integer|unique:users',
             'password' => 'required|string|min:6|confirmed',
         ]);
-
         if ($validator->fails()) {
-            return response()->json(['error' => $validator->errors()], Response::HTTP_UNAUTHORIZED);            
+            return response()->json(['error' => $validator->errors()], Response::HTTP_UNAUTHORIZED);
         }
-        
         $data['password'] = bcrypt($data['password']);
         $user = User::create($data);
         $success['token'] =  $user->createToken('MyApp')->accessToken;
