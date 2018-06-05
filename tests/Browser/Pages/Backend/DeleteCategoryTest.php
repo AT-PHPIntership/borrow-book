246cc4b5c76b8a6d6ec9ac3076fc861b40d26470
Thanh Ha <ha.phan@asiantech.vn>
2018-06-05 16:57:46 +0700

    unit test delete category

--- tests/Browser/Pages/Backend/DeleteCategoryTest.php
+++ tests/Browser/Pages/Backend/DeleteCategoryTest.php
@@ -33,8 +33,54 @@ class DeleteCategoryTest extends DuskTestCase
         factory(Post::class)->create();
         factory(Favorite::class)->create();
         factory(Rating::class)->create();
-        factory(Borrow::class)->create();
-        factory(BorrowDetail::class)->create();
+        factory(Borrow::class,1)->create();
+        $borrowId = DB::table('borrowes')->pluck('id')->all();
+        $faker = Faker::create();
+        factory(BorrowDetail::class,2)->create([
+            'borrow_id' => $faker->randomElement($borrowId),
+        ]);
     }
 
+    /**
+     * Test button delete book in list category
+     *
+     * @return void
+     */
+    public function testButtonCancelDeleleUser()
+    {
+        $this->browse(function (Browser $browser) {
+            $browser->loginAs($this->user)
+                ->visit('/admin/categories')
+                ->assertSee('List Category')
+                ->click('.btn-delete-item')
+                ->assertDialogOpened('Are you sure?')
+                ->dismissDialog();
+            $this->assertDatabaseHas('categories', ['id' => 1, 'deleted_at' => null]);
+        });
+    }
+
+    /**
+     * Test click button Delete
+     *
+     * @return void
+     */
+    public function testAppectDeleleUser()
+    {
+        $this->browse(function (Browser $browser) {
+            $browser->loginAs($this->user)
+                ->visit('/admin/categories')
+                ->click('.btn-delete-item')
+                ->assertDialogOpened('Are you sure?')
+                ->acceptDialog()
+                ->pause(1000)
+                ->assertSee('Delete Category Successfully!');
+            $this->assertDatabaseMissing('categories', ['id' => 1, 'deleted_at' => null])
+                ->assertDatabaseMissing('posts', ['id'=> 1, 'deleted_at' => null])
+                ->assertDatabaseMissing('ratings', ['id'=> 1])
+                ->assertDatabaseMissing('favorites', ['id'=> 1])
+                ->assertDatabaseMissing('borrowes', ['id'=> 1, 'deleted_at' => null])
+                ->assertDatabaseMissing('borrow_details', ['id'=> 1])
+                ->assertDatabaseMissing('books', ['id'=> 1, 'deleted_at' => null]);
+        });
+    }
 }
