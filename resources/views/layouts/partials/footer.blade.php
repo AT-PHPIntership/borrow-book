f2a46d1f2d3e5e3ee4cea2fb1566996301329011
Hay Tran <haytranngoc@gmail.com>
2018-06-05 09:30:59 +0700

    create layout master page, header, footer

---
+++ resources/views/layouts/partials/footer.blade.php
@@ -0,0 +1,27 @@
+<footer id="colorlib-footer" role="contentinfo">
+    <div class="container">
+        <div class="row row-pb-md">
+            <div class="col-md-3 colorlib-widget">
+                <h4></h4>
+                <p>
+                    <ul class="colorlib-social-icons">
+                    </ul>
+                </p>
+            </div>
+            <div class="col-md-2 colorlib-widget">
+                <h4></h4>
+                <p>
+                    <ul class="colorlib-footer-links">
+                        <li><a href="#"></a></li>
+                    </ul>
+                </p>
+            </div>
+        </div>
+    </div>
+    <div class="copy">
+        <div class="row">
+            <div class="col-md-12 text-center">
+            </div>
+        </div>
+    </div>
+</footer>
