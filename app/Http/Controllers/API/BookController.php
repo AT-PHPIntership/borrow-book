75ad121e4563407be9166dfe01f44841a5eef287
at-nale <na.le@asiantech.vn>
2018-06-08 10:07:34 +0700

    add api search book

--- app/Http/Controllers/API/BookController.php
+++ app/Http/Controllers/API/BookController.php
@@ -9,14 +9,23 @@ use App\Models\Book;
 
 class BookController extends ApiController
 {
-    /**
-     * Display a listing of the resource.
+   /**
+     * Get api list books, search
+     *
+     * @param Request $request request
      *
      * @return \Illuminate\Http\Response
      */
-    public function index()
+    public function index(Request $request)
     {
-        $book = Book::with(['category', 'imageBooks'])->get();
-        return $this->showAll($book, Response::HTTP_OK);
+        $books = Book::with(['category', 'imageBooks'])
+                ->whereHas('category', function ($query) use ($request) {
+                    $query->where('name', 'like', "%$request->search%");
+                })
+                ->orWhere('title', 'like', "%$request->search%")
+                ->orWhere('author', 'like', "%$request->search%")
+                ->orWhere('language', 'like', "%$request->search%")
+                ->get();
+        return $this->showAll($books, Response::HTTP_OK);
     }
 }
