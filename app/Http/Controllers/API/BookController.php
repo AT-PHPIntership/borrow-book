f6d0442c292e057fef2ab8675be7ef60b582add4
at-nale <na.le@asiantech.vn>
2018-06-08 08:24:05 +0700

    fix comment

--- app/Http/Controllers/API/BookController.php
+++ app/Http/Controllers/API/BookController.php
@@ -5,12 +5,7 @@ namespace App\Http\Controllers\API;
 use Illuminate\Http\Request;
 use App\Http\Controllers\ApiController;
 use Symfony\Component\HttpFoundation\Response;
-use App\Models\User;
-use App\Http\Requests\Api\RegisterRequest;
-use Auth;
 use App\Models\Book;
-use App\Models\Category;
-use DB;
 
 class BookController extends ApiController
 {
@@ -21,7 +16,7 @@ class BookController extends ApiController
      */
     public function index()
     {
-        $book = Book::with('category', 'imageBooks')->get();
+        $book = Book::with(['category', 'imageBooks'])->get();
         return $this->showAll($book, Response::HTTP_OK);
     }
 }
