70d80d8cf106df4273bb34017b8886f90632c254
Hay Tran <haytranngoc@gmail.com>
2018-06-13 15:53:36 +0700

    fix comment

--- public/js/showListBook.js
+++ public/js/showListBook.js
@@ -23,30 +23,7 @@ function getListBooks() {
                 }
             });
         }
-        var books = '';
-        $.each(data.data, function (key, value) {
-            if(typeof value.image_books[0] === 'undefined') {
-                value.image_books[0] = {
-                    'image': 'http://via.placeholder.com/150x150'
-                };
-            }
-            books += '<div class="col-md-3 text-center" >\
-                        <div class="product-entry">\
-                            <div class="product-img" style="background-image: url('+ value.image_books[0].image +');">\
-                                <p class="tag"><span class="sale">'+ value.author +'</span></p>\
-                                <div class="cart">\
-                                    <p>\
-                                        <span><a href="#"></a></span>\
-                                    </p>\
-                                </div>\
-                            </div>\
-                            <div class="desc">\
-                                <h3><a href="#">'+ value.title +'</a></h3>\
-                            </div>\
-                        </div>\
-                    </div>';
-            $("#books").html(books);
-        });
+        contentBook(data);
     })
     .fail(function(data) {
         if (data.responseJSON.message) {
@@ -56,5 +33,31 @@ function getListBooks() {
             window.alert(data.responseJSON);
         }
     });
-    
 }
+
+function contentBook(data) {
+    var books = '';
+    $.each(data.data, function (key, value) {
+        if(typeof value.image_books[0] === 'undefined') {
+            value.image_books[0] = {
+                'image': 'http://via.placeholder.com/150x150'
+            };
+        }
+        books += '<div class="col-md-3 text-center" >\
+                    <div class="product-entry">\
+                        <div class="product-img" style="background-image: url('+ value.image_books[0].image +');">\
+                            <p class="tag"><span class="sale">'+ value.author +'</span></p>\
+                            <div class="cart">\
+                                <p>\
+                                    <span><a href="#"></a></span>\
+                                </p>\
+                            </div>\
+                        </div>\
+                        <div class="desc">\
+                            <h3><a href="#">'+ value.title +'</a></h3>\
+                        </div>\
+                    </div>\
+                </div>';
+        $("#books").html(books);
+    });
+} 
