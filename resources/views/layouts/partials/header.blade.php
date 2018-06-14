7d79249457d26a9f69b71408d351bf3517379b4e
Thanh Ha <ha.phan@asiantech.vn>
2018-06-14 17:04:18 +0700

    use api show category

--- resources/views/layouts/partials/header.blade.php
+++ resources/views/layouts/partials/header.blade.php
@@ -8,14 +8,9 @@
                 <div class="col-xs-10 text-right menu-1">
                     <ul>
                         <li class="active"><a href="#">Home</a></li>
-                        <li class="has-dropdown">
-                            <a href=""></a>
-                            <ul class="dropdown">
-                                <li><a href="#"></a></li>
-                                <li><a href="#"></a></li>
-                                <li><a href="#"></a></li>
-                                <li><a href="#"></a></li>
-                                <li><a href="#"></a></li>
+                        <li class="dropdown">
+                            <a href="javascript:void(0);" class="category dropdown-toggle" id="menu1" data-toggle="dropdown">Categories<span class="caret"></span></a>
+                            <ul class="dropdown-menu category-list" role="menu" aria-labelledby="menu1">
                             </ul>
                         </li>
                         <li><a href=""></a></li>
