40f3142e9c0c70ca3aa66e06929d4eb6ab93475b
Thanh Ha <ha.phan@asiantech.vn>
2018-06-15 15:25:55 +0700

    api create posts

--- app/Http/Controllers/API/PostController.php
+++ app/Http/Controllers/API/PostController.php
@@ -7,6 +7,10 @@ use App\Http\Controllers\ApiController;
 use Symfony\Component\HttpFoundation\Response;
 use App\Models\Post;
 use App\Models\Book;
+use App\Http\Requests\CreatePostRequest;
+use Auth;
+use DB;
+use App\Models\User;
 
 class PostController extends ApiController
 {
@@ -27,4 +31,40 @@ class PostController extends ApiController
         $posts = $posts->with(['user'])->where('book_id', $book->id)->get();
         return $this->showAll($posts, Response::HTTP_OK);
     }
-}
+
+    /**
+     * Api store new posst
+     *
+     * @param \App\Models\Book                     $book    book of this post
+     * @param \App\Http\Requests\CreatePostRequest $request request
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Book $book, CreatePostRequest $request)
+    {
+        $user = Auth::user();
+        $post = new Post;
+        DB::beginTransaction();
+        try {
+            $post->user_id = $user->id;
+            $post->book_id = $book->id;
+            $post->body = $request->body;
+            if (isset($request->rate_point)) {
+                $post->rate_point = $request->rate_point;
+                $post->post_type = Post::REVIEW;
+                $book->total_rate += $request->rate_point;
+                $book->count_rate += 1;
+                $book->save();
+            } else {
+                $post->post_type = Post::COMMENT;
+            }
+            $post->status = 0;
+            $post->save();
+            DB::commit();
+            return $this->showOne($post->load('user'), Response::HTTP_OK);
+        } catch (Exception $e) {
+            DB::rollBack();
+            throw new ModelNotFoundException();
+        }
+    }
+ }
