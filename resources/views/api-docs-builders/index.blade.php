44466c869bf769964345c599afc5d7f2426adf64
at-nale <na.le@asiantech.vn>
2018-06-18 15:33:45 +0700

    docs profile user

--- resources/views/api-docs-builders/index.blade.php
+++ resources/views/api-docs-builders/index.blade.php
@@ -3,3 +3,4 @@
 {!! file_get_contents(base_path('docs/api-references/book.md')) !!}
 {!! file_get_contents(base_path('docs/api-references/category.md')) !!}
 {!! file_get_contents(base_path('docs/api-references/post.md')) !!}
+{!! file_get_contents(base_path('docs/api-references/profile.md')) !!}
