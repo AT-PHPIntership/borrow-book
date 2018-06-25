fa651a841d4c1641d75732fe1c087181adfa005a
Thanh Ha <ha.phan@asiantech.vn>
2018-06-25 10:51:21 +0700

    update layout

--- resources/lang/en/homepage.php
+++ resources/lang/en/homepage.php
@@ -8,4 +8,23 @@
             'decription' => 'The new books is arrival'
         ],
         'show_more' => 'Show more...',
+        'banner' => [
+            'banner_1' => [
+                'head_1' => 'More Books',
+                'head_2' => 'Are Arrived',
+                'tab' => 'New Books',
+            ],
+            'banner_2' => [
+                'head_1' => 'Top 10',
+                'head_2' => 'Bestselling books',
+                'head_3' => 'In May',
+                'tab' => 'Book Collections',
+            ],
+            'banner_3' => [
+                'head_1' => 'More books',
+                'head_2' => 'More deals',
+                'category' => 'In Summer Book Festival',
+                'tab' => 'Book festivals',
+            ],
+        ]
     ];
