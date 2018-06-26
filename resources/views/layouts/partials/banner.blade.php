9433b729a39059bebca89a6f68441686a9c96327
Thanh Ha <ha.phan@asiantech.vn>
2018-06-26 11:01:41 +0700

    fix layout

--- resources/views/layouts/partials/banner.blade.php
+++ resources/views/layouts/partials/banner.blade.php
@@ -1,53 +1,50 @@
 <aside id="colorlib-hero">
     <div class="flexslider">
         <ul class="slides">
-            <li style="background-image: url(http://via.placeholder.com/350x150);">
+            <li style="background-image: url(http://192.168.33.10/images/banner1.png);">
                 <div class="overlay"></div>
                 <div class="container-fluid">
                     <div class="row">
                         <div class="col-md-6 col-md-offset-3 col-md-pull-2 col-sm-12 col-xs-12 slider-text">
                             <div class="slider-text-inner">
                                 <div class="desc">
-                                    <h1 class="head-1"></h1>
-                                    <h2 class="head-2"></h2>
-                                    <p class="category"><span></span></p>
-                                    <p><a href="#" class="btn btn-primary"></a></p>
+                                    <h2 class="head-1">{{ trans('homepage.banner.banner_1.head_1') }}</h2>
+                                    <h3 class="head-2">{{ trans('homepage.banner.banner_1.head_2') }}</h3>
+                                    <p><a href="#" class="btn btn-primary">{{ trans('homepage.banner.banner_1.tab') }}</a></p>
                                 </div>
                             </div>
                         </div>
                     </div>
                 </div>
             </li>
-            <li style="background-image: url(http://via.placeholder.com/350x150);">
+            <li style="background-image: url(http://192.168.33.10/images/banner2.jpg);">
                 <div class="overlay"></div>
                 <div class="container-fluid">
                     <div class="row">
                         <div class="col-md-6 col-md-offset-3 col-md-pull-2 col-sm-12 col-xs-12 slider-text">
                             <div class="slider-text-inner">
                                 <div class="desc">
-                                    <h1 class="head-1"></h1>
-                                    <h2 class="head-2"></h2>
-                                    <h2 class="head-3"></h2>
-                                    <p class="category"><span></span></p>
-                                    <p><a href="#" class="btn btn-primary"></a></p>
+                                    <h2 class="head-1">{{ trans('homepage.banner.banner_2.head_1') }}</h2>
+                                    <h3 class="head-2">{{ trans('homepage.banner.banner_2.head_2') }}</h3>
+                                    <h3 class="head-3">{{ trans('homepage.banner.banner_2.head_3') }}</h3>
+                                    <p><a href="#" class="btn btn-primary">{{ trans('homepage.banner.banner_2.tab') }}</a></p>
                                 </div>
                             </div>
                         </div>
                     </div>
                 </div>
             </li>
-            <li style="background-image: url(http://via.placeholder.com/350x150);">
+            <li style="background-image: url(http://192.168.33.10/images/banner3.jpg);">
                 <div class="overlay"></div>
                 <div class="container-fluid">
                     <div class="row">
                         <div class="col-md-6 col-md-offset-3 col-md-push-3 col-sm-12 col-xs-12 slider-text">
                             <div class="slider-text-inner">
                                 <div class="desc">
-                                    <h1 class="head-1"></h1>
-                                    <h2 class="head-2"></h2>
-                                    <h2 class="head-3"></h2>
-                                    <p class="category"><span></span></p>
-                                    <p><a href="#" class="btn btn-primary"></a></p>
+                                    <h2 class="head-1">{{ trans('homepage.banner.banner_3.head_1') }}</h2>
+                                    <h3 class="head-2">{{ trans('homepage.banner.banner_3.head_2') }}</h3>
+                                    <p class="category"><span>{{ trans('homepage.banner.banner_3.category') }}</span></p>
+                                    <p><a href="#" class="btn btn-primary">{{ trans('homepage.banner.banner_3.tab') }}</a></p>
                                 </div>
                             </div>
                         </div>
