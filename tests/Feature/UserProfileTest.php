62394dc3103c1595f3f11bcaecc2e45b24df2b07
at-nale <na.le@asiantech.vn>
2018-06-27 09:29:48 +0700

    Unitest profile user

---
+++ tests/Feature/UserProfileTest.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use App\Models\User;
+use Illuminate\Http\Response;
+
+class UserProfileTest extends TestCase
+{
+    use DatabaseMigrations;
+
+    /**
+     * Return structure of json.
+     *
+     * @return array
+     */
+    public function jsonStructureProfileUser()
+    {
+        return [
+            'data' => [
+                "id",
+                "name",
+                "email",
+                "identity_number",
+                "avatar",
+                "dob",
+                "address",
+                "role"
+            ]
+        ];
+    }
+     /**
+     * Test structure of json response.
+     *
+     * @return void
+     */
+    public function testJsonProfileUser()
+    {
+
+        $response = $this->jsonUser('GET', '/api/users/profile');
+        $response->assertJsonStructure($this->jsonStructureProfileUser());
+        $response->assertStatus(Response::HTTP_OK);
+    }
+
+    /**
+     * Test compare database
+     * 
+     * @return void
+     */
+    public function testCompareDatabase()
+    {
+        $response = $this->jsonUser('GET', '/api/users/profile');
+        $data = json_decode($response->getContent());
+        $arrayCompare = [
+            'id' => $data->data->id,
+            'name' => $data->data->name,
+            'email' => $data->data->email,
+            'identity_number' => $data->data->identity_number,
+            'dob' => $data->data->dob,
+            'address' => $data->data->address,
+            'role' => $data->data->role,
+        ];
+        $this->assertDatabaseHas('users', $arrayCompare);
+    }
+
+    /**
+     * Test get profile of user when unauthorized
+     *
+     * @return void
+     */
+    public function testUnauthorized()
+    {
+        factory(User::class)->create();        
+        $response = $this->json('GET', '/api/users/profile');
+        $response->assertStatus(Response::HTTP_UNAUTHORIZED);
+    }
+}
--- tests/TestCase.php
+++ tests/TestCase.php
@@ -3,8 +3,35 @@
 namespace Tests;
 
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
+use Illuminate\Support\Facades\Artisan;
 
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
+    protected $user;
+    protected $token;
+    /**
+     * Set up TestCase
+     *
+     * @return void
+     */
+    public function setUp()
+    {
+        parent::setUp();
+        Artisan::call('passport:install');
+        $this->user = factory('App\Models\User')->create();
+        $this->access_token =  $this->user->createToken('access_token')->accessToken;
+    }
+    /**
+     * Get json response
+     *
+     * @return json
+     */
+    public function jsonUser($method, $url, $data = [], $header = [])
+    {
+        if ($header) {
+            return $this->json($method, $url, $data, $header);
+        }
+        return $this->json($method, $url, $data, ['Accept' => 'application/json', 'Authorization' => 'Bearer '.$this->access_token]);
+    }
 }
