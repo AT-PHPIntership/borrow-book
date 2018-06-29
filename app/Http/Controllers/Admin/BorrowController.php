7389c96c3499bb4298f967b0e7aa4db4dc7d6956
Hay Tran <haytranngoc@gmail.com>
2018-06-29 20:18:23 +0700

    fix send mail

--- app/Http/Controllers/Admin/BorrowController.php
+++ app/Http/Controllers/Admin/BorrowController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers\Admin;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Models\Borrow;
+use App\Mail\UpdateStatusBorrowMail;
+use Mail;
 
 class BorrowController extends Controller
 {
@@ -31,6 +33,12 @@ class BorrowController extends Controller
     {
         $borrow['status'] = $request->status;
         $borrow->save();
+        $data['name'] = $borrow->user->name;
+        $data['from_date'] = $borrow->from_date;
+        $data['to_date'] = $borrow->to_date;
+        $data['status'] = $borrow->status;
+        $data['borrow_details'] = $borrow->borrowDetails;
+        Mail::to($borrow->user->email)->send(new UpdateStatusBorrowMail($data));
         return response()->json($borrow);
     }
 }
