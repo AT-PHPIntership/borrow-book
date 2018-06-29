9e9d38a4a5c789f58005f906d1732e0ead46ac87
Hay Tran <haytranngoc@gmail.com>
2018-06-29 10:04:57 +0700

    update layout show detail book

--- resources/views/layouts/partials/footer.blade.php
+++ resources/views/layouts/partials/footer.blade.php
@@ -1,19 +1,32 @@
+ @if (!request()->is('login') && !request()->is('register') && !request()->is('cart'))
+<div id="colorlib-subscribe">
+    <div class="overlay"></div>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8 col-md-offset-2">
+                <div class="col-md-6 text-center">
+                </div>
+                <div class="col-md-6">
+                    <form class="form-inline qbstp-header-subscribe">
+                        <div class="row">
+                            <div class="col-md-12 col-md-offset-0">
+                                <div class="form-group">
+                                </div>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endif
 <footer id="colorlib-footer" role="contentinfo">
     <div class="container">
         <div class="row row-pb-md">
-            <div class="col-md-3 colorlib-widget">
-                <h4></h4>
-                <p>
-                    <ul class="colorlib-social-icons">
-                    </ul>
-                </p>
-            </div>
             <div class="col-md-2 colorlib-widget">
                 <h4></h4>
                 <p>
-                    <ul class="colorlib-footer-links">
-                        <li><a href="#"></a></li>
-                    </ul>
                 </p>
             </div>
         </div>
@@ -25,3 +38,6 @@
         </div>
     </div>
 </footer>
+<div class="gototop js-top active">
+    <a href="#" class="js-gotop"><i class="fa fa-arrow-up"></i></a>
+</div>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,5 +1,31 @@
 @extends('layouts.master')
 @section('content')
+    <div id="colorlib-featured-product">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-6">
+                    <a href="{{ route('books.index') }}" class="f-product-1" style="background-image: url({{ asset('/images/product1.jpg') }});">
+                    </a>
+                </div>
+                <div class="col-md-6">
+                    <div class="row">
+                        <div class="col-md-6">
+                            <a href="{{ route('books.index') }}" class="f-product-2" style="background-image: url({{ asset('/images/product2.jpg') }});">
+                            </a>
+                        </div>
+                        <div class="col-md-6">
+                            <a href="{{ route('books.index') }}" class="f-product-2" style="background-image: url({{ asset('/images/product3.jpg') }});">
+                            </a>
+                        </div>
+                        <div class="col-md-12">
+                            <a href="{{ route('books.index') }}" class="f-product-2" style="background-image: url({{ asset('/images/product4.jpg') }});">
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
     <div class="colorlib-shop">
         <div class="container">
             <div class="row">
@@ -12,6 +38,31 @@
                 
             </div>
             <div class="text-center"><a href="{{ route('books.index') }}" id="show-more">{{ trans('homepage.show_more') }}</a></div>
+        </div>
+    </div>
+    <div id="colorlib-intro" class="colorlib-intro" style="background-image: url({{ asset('/images/product2.jpg') }}); background-position: 50% 71px;" data-stellar-background-ratio="0.5">
+        <div class="overlay"></div>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-6">
+                    <div class="intro-desc">
+                        <div class="text-salebox">
+                            <div class="text-lefts">
+                                <div class="sale-box">
+                                    <div class="sale-box-top">
+                                        <h2 class="number">{{ trans('homepage.relax') }}</h2>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="text-rights">
+                                <h3 class="title">{{ trans('homepage.banner.banner_3.category') }}</h3>
+                                <p>{{ __('Separated they live in Bookmarksgrove right at the coast of the Semantics, a large language ocean.') }}</p>
+                                <p><a href="{{ route('books.index') }}" class="btn btn-primary btn-outline">{{ trans('homepage.banner.banner_1.head_1') }}</a></p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
 @endsection
