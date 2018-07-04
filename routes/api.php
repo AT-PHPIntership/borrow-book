53605d7f72f645fbc0b21ad53b8db9caf4a11b7b
Hay Tran <haytranngoc@gmail.com>
2018-07-04 11:25:52 +0700

    api cancel borrow

--- routes/api.php
+++ routes/api.php
@@ -24,6 +24,7 @@ Route::group(['middleware' => 'auth:api'], function() {
     Route::put('posts/{post}', 'API\PostController@update');
     Route::post('logout','API\LoginController@logout');
     Route::post('borrow','API\BorrowController@store');
+    Route::put('borrow/{borrow}','API\BorrowController@cancel');
     Route::get('users/profile','API\UserController@profile');
     Route::get('users/posts', 'API\UserController@getPost');
     Route::get('users/borrow', 'API\BorrowController@index');
