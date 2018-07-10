ea111b91b0bd573f4566660f8f932ec35d909af1
Thanh Ha <ha.phan@asiantech.vn>
2018-07-10 10:21:08 +0700

    use api update post profile

--- public/js/userPost.js
+++ public/js/userPost.js
@@ -1,6 +1,11 @@
 var delete_confirm = Lang.get('auth.messages.delete_confirm');
 var delete_success = Lang.get('auth.messages.delete_success');
 var url = '/api/users/posts?limit=8';
+var urlUpdatePost = '/api/posts/';
+const COMMENT = 0;
+const REVIEW = 1;
+const ACCEPT = 1;
+const UNACCEPT = 0;
 
 function getUserPosts() {
     $.ajax({
@@ -11,10 +16,6 @@ function getUserPosts() {
             'Authorization': 'Bearer ' + localStorage.getItem('access_token'),
         },
         success: function(response) {
-            const COMMENT = 0;
-            const REVIEW = 1;
-            const ACCEPT = 1;
-            const UNACCEPT = 0;
             if (response['next_page_url'] != null) {
                 $('#next').show();
                 $('#next').attr('href', response['next_page_url']);
@@ -26,8 +27,9 @@ function getUserPosts() {
                 let rate = posts.rate_point;
                 let nameBook = posts.book.title;
                 let idpost = posts.id;
+                let postType = posts.post_type;
                 let type = 'Comment';
-                $('#template-post').clone().attr({"style":"display: ", "id":idpost}).insertBefore('#template-post');
+                $('#template-post').clone().attr({ "style": "display: ", "id": idpost, "data-post-type": postType }).insertBefore('#template-post');
                 $("#"+ idpost +" .body").text(body);
                 $("#"+ idpost +" .book-name").text(nameBook);
                 if (posts.status == ACCEPT) {
@@ -35,13 +37,13 @@ function getUserPosts() {
                 } else {
                     $("#"+ idpost +" .status .btn-posts-success").hide();
                 }
-                if (posts.post_type == REVIEW) {
+                if (postType == REVIEW) {
                     type = "Review";
                     $("#"+ idpost +" .rate").text(rate);
                 }
                 $("#"+ idpost +" .type").html(type);
                 $("#"+ idpost +" .option .delete-post-user").attr('id', idpost);
-                $(".update-post-user").attr('data-id', idpost);
+                $("#"+ idpost +" .update-post-user").attr('data-id', idpost);
             });
         }
     });
@@ -80,23 +82,124 @@ function deletePostUser() {
 }
 deletePostUser();
 
-function editCommentProfile() {
-    $("#form-edit-post").dialog({
-        autoOpen: false,
-        show: 'slide',
-        resizable: false,
-        position: 'center',
-        stack: true,
-        height: 'auto',
-        width: 'auto',
-        modal: true
+$(document).ready(function(){
+    $('#stars li').on('mouseover', function(){
+        var onStar =$(this).data('value');
+        $(this).parent().children('li.star').each(function(e){
+            if (e < onStar) {
+                $(this).addClass('hover');
+            } else {
+                $(this).removeClass('hover');
+            }
+        });
+    }).on('mouseout', function(){
+        $(this).parent().children('li.star').each(function(e){
+            $(this).removeClass('hover');
+        });
     });
-    $('.update-post-user').on('click', function () {
-        $('').attr('class', 'center');
-    })
+    $('#stars li').on('click', function(){
+        var onStar = $(this).data('value');
+        var stars = $(this).parent().children('li.star');
+        for (i = 0; i < stars.length; i++) {
+            $(stars[i]).removeClass('selected');
+        }
+        for (i = 0; i < onStar; i++) {
+          $(stars[i]).addClass('selected');
+        }
+    }); 
+});
 
-    $('#close').on('click', function () {
-        $('#form-edit-post').hide();
-    })
+function editPostProfile() {
+    var postId, postType;
+    $(document).on('click', '.update-post-user',function() {
+        postId = $(this).attr('data-id');
+        postType = $('#'+ postId).attr('data-post-type');
+        if(postType == REVIEW) {
+            var rateStars = $("#"+ postId +" .rate").html();
+            var stars = $('#stars li').parent().children('li.star');
+            for (var i = 0; i < rateStars; i++) {
+                $(stars[i]).addClass('selected');
+            }
+            $('#content-post').val($("#"+ postId +" .body").html());
+            editReview(postId);
+        } else{
+            $('.rating-stars').attr('class', 'hidden');
+            $('#content-post').val($("#"+ postId +" .body").html());
+            editComment(postId);
+        }
+    });
+      
 }
-editCommentProfile();
+editPostProfile();
+
+function editComment(postId) {
+    $(document).on('click', '#submit-update-post', function(event) {
+        $.ajax({
+            url: urlUpdatePost + postId,
+            type: 'PUT',
+            headers: {
+                'Accept': 'application/json',
+                'Authorization': 'Bearer ' + localStorage.getItem('access_token'),
+            },
+            data: {
+                post_type: COMMENT,
+                body: $('#content-post').val(),
+            },
+            success: function(data) {
+                $('.review_success').show();
+                $("#"+ postId +" .body").text($('#content-post').val());
+                $('#content_review').val('');
+                $("#"+ postId +" .status .btn-posts-success").hide();
+                $('#modal-update-post').modal('hide');
+            },
+            error: function(data) {
+                errorMessage = data.responseJSON.message + '<br/>';
+                if (data.responseJSON.errors) {
+                    errors = Object.keys(data.responseJSON.errors);
+                    errors.forEach(error => {
+                        errorMessage += data.responseJSON.errors[error] + '<br/>';
+                    });
+                }
+                $('.review_error').html(errorMessage);
+                $('.review_error').show();
+            }
+        });
+    });  
+}
+
+function editReview(postId) {
+    $(document).on('click', '#submit-update-post', function(event) {
+        $.ajax({
+            url: urlUpdatePost + postId,
+            type: 'PUT',
+            headers: {
+                'Accept': 'application/json',
+                'Authorization': 'Bearer ' + localStorage.getItem('access_token'),
+            },
+            data: {
+                post_type: REVIEW,
+                rate_point: $('#stars li.selected').last().data('value'),
+                body: $('#content-post').val(),
+            },
+            success: function(data) {
+                $('.review_success').show();
+                $("#"+ postId +" .body").text($('#content-post').val());
+                $('#content_review').val('');
+                $("#"+ postId +" .rate").text($('#stars li.selected').last().data('value'));
+                $("#"+ postId +" .status .btn-posts-success").hide();
+                $('#modal-update-post').modal('hide');
+            },
+            error: function(data) {
+                errorMessage = data.responseJSON.message + '<br/>';
+                if (data.responseJSON.errors) {
+                    errors = Object.keys(data.responseJSON.errors);
+                    errors.forEach(error => {
+                        errorMessage += data.responseJSON.errors[error] + '<br/>';
+                    });
+                }
+                $('.review_error').html(errorMessage);
+                $('.review_error').show();
+            }
+        });
+    });
+}
