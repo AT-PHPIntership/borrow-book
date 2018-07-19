ce3460cbc0514a1e82bc37555d1410b9fd12ab0c
at-nale <na.le@asiantech.vn>
2018-07-19 10:50:19 +0700

    add case status from READ to UNREAD

--- public/js/userFavorite.js
+++ public/js/userFavorite.js
@@ -33,25 +33,34 @@ function getListFavorite() {
     });
 }
 function updateStatus() {
-    $(document).on('click', '.btn-status',function(event)  {
+    $(document).on('click', '.btn-status',function(event) {
         var favoriteId = $(this).attr('data-id');
         var status = $(this).attr('data-status');
-        if(status == UNREAD) {
-            $.ajax({
-                url: '/api/users/favorites/' + favoriteId,
-                type: 'PUT',
-                headers: {
-                    'Accept': 'application/json',
-                    'Authorization': 'Bearer ' + localStorage.getItem('access_token'),
-                },
-                data: {
-                    status: READ
-                },
-                success: function(data) {
+        if(status == READ) {
+            status = UNREAD
+        } else {
+            status = READ
+        }
+        $.ajax({
+            url: '/api/users/favorites/' + favoriteId,
+            type: 'PUT',
+            headers: {
+                'Accept': 'application/json',
+                'Authorization': 'Bearer ' + localStorage.getItem('access_token'),
+            },
+            data: {
+                status: status
+            },
+            success: function(data) {
+                if(data.status == READ) {
                    $("#favorite"+ favoriteId +" .status .btn-read").show();
                    $("#favorite"+ favoriteId +" .status .btn-unread").hide();
-                },
-            });
-        }
+                } else {
+                    $("#favorite"+ favoriteId +" .status .btn-unread").show();
+                    $("#favorite"+ favoriteId +" .status .btn-read").hide();
+                }
+            }
+        });
+        
     });  
 }
